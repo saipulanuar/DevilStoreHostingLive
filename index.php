312d01452b3f05af0a6ffa02e5a6e10c270831ab
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2021-01-09 16:53:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,168 @@
+<<html>
+
+<!-- Mirrored from kelibohosted.tk/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 08 Jan 2021 08:21:35 GMT -->
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<audio src="https://e.top4top.io/m_18325ili30.mp3" autoplay="autoplay" hidden="hidden"></audio>
+<body background="images/bg.jpg">
+  
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href='external9143.html' rel='icon' type='image/x-png'/>
+<title>SAIPUL | DEMO PHISING </title>
+<link rel="stylesheet" href="style.css" type="text/css"/>
+<meta property="og:image" content="images/gh.html">
+
+</head><body style="padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; margin-top: 0px; margin-right: auto; margin-bottom: 0px; margin-left: auto; cursor: default;"><div align="center"><noscript>
+<div style="position:fixed; top:0px; left:0px; z-index:3000; height:100%; width:100%; background-color:#FFFFFF">
+<div style="font-family: Arial; font-size: 17px; background-color:#00bbf9; padding: 11pt;"> Mohon aktifkan javascript pada browser untuk mengakses halaman ini! </div></div>
+<!-- SCM Music Player https://scmplayer.co -->
+<script type="text/javascript" src="script.js" data-config="{'skin':'skins/tunes/skin.css','volume':62,'autoplay':true,'shuffle':false,'repeat':1,'placement':'top','showplaylist':false,'playlist':[{'title':'Faded - Alan Walker - DEF%u039BLT Edit (Extended Editon)','url':'https://soundcloud.com/defaltmx/faded-alan-walker-datpikayt-remix-extended-editon'}]}"></script>
+<!-- SCM Music Player script end -->
+</noscript></div>
+<!--Kode untuk mencegah seleksi teks, block teks dll.-->
+<script type="text/javascript">
+function disableSelection(e){if(typeof e.onselectstart!="undefined")e.onselectstart=function(){return false};else if(typeof e.style.MozUserSelect!="undefined")e.style.MozUserSelect="none";else e.onmousedown=function(){return false};e.style.cursor="default"}window.onload=function(){disableSelection(document.body)}
+</script>
+<!--Kode untuk mematikan fungsi klik kanan di blog-->
+<script type="text/javascript">
+function mousedwn(e){try{if(event.button==2||event.button==3)return false}catch(e){if(e.which==3)return false}}document.oncontextmenu=function(){return false};document.ondragstart=function(){return false};document.onmousedown=mousedwn
+</script>
+<style type="text/css">* : (input, textarea) {-webkit-touch-callout:none;-webkit-user-select:none}</style>
+<style type="text/css">img{-webkit-touch-callout:none;-webkit-user-select:none}</style>
+<!--Kode untuk mencegah shorcut keyboard, view source dll.-->
+<script type="text/javascript">
+window.addEventListener("keydown",function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){e.preventDefault()}});document.keypress=function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){}return false}
+</script>
+<script type="text/javascript">
+document.onkeydown=function(e){e=e||window.event;if(e.keyCode==123||e.keyCode==18){return false}}
+</script>
+<marquee behavior="alternate" scrollamount="3" onmouseover="this.stop()" onmouseout="this.start()">
+<a href="https://wa.me/6281554414321"><font color="red"><b>READY </font>Web Phising For Mobile Legends</b></a></marquee>
+
+<div class="hr"></div>
+<fieldset class=important><legend><font color="aqua"><b>
+<div id="angelhost">SAIPUL WEBHOST</div>
+
+</b></font></legend><center>
+<b><script language="javascript" src="js/angelhost-ondate.html"></script></b>
+<br>
+<br>
+
+<font color="white"><b>ORDER KLIK DISINI</font> : <a href="https://wa.me/6281554414321" target="_blank">SAIPUL</a></b>
+
+<br>
+<br>
+
+</center></fieldset>
+
+<div class="hr"></div></div>
+
+<div class="header"></div>
+<div class="contents">
+
+<div class="mid_menu"></div>
+<div class="contents">
+<div class="hr"></div>
+<center><font color="white"><b> ALL DEMO MOBILE LEGENDS </b></font></center>
+<div class="hr"></div>
+<div class="top_menufoot"></div><div class="mid_menu"></div>
+<div class="contents">
+<div class="hr"></div>
+<center><img src="images/11.gif"><font color="white"><b> TAMPILAN SKIN & DIAMOND </b></font><img src="images/12.gif"></center>
+<div class="hr"></div>
+<img src="images/idn.gif"><a href="tampilan/skin-M2.jpg"> MLBB SKIN M2</a> <img src="images/new.gif"><br>
+<div class="hr"></div>
+<img src="images/idn.gif"><a href="tampilan/m2.jpg"> MLBB NEW M2</a> <img src="images/new.gif"><br>
+<div class="hr"></div>
+<img src="images/idn.gif"><a href="../i.ibb.co/C0CXjzg/MLBB-V1.jpg"> MLBB V1</a> <img src="images/new.gif"><br>
+<div class="hr"></div>
+<img src="images/idn.gif"><a href="../i.ibb.co/2nm1YxF/MLBB-V2.jpg"> MLBB V2</a> <img src="images/new.gif"><br>
+<div class="hr"></div>
+<img src="images/idn.gif"><a href="../i.ibb.co/m4wmh5h/Codashop-New-Valir.jpg"> MLBB CODASHOP V1</a> <img src="images/new.gif"><br>
+<div class="hr"></div>
+<img src="images/idn.gif"><a href="tampilan/Codashop-V2.jpg"> MLBB CODASHOP V2</a> <img src="images/new.gif"><br>
+<div class="hr"></div>
+<img src="images/idn.gif"><a href="../i.ibb.co/ymCJQr4/Mlbb-Spin.jpg"> MAGIC WHELL SPIN</a> <img src="images/new.gif"><br>
+<div class="hr"></div>
+<img src="images/idn.gif"><a href="../i.ibb.co/CvTRFQc/Screenshot-2021-01-04-23-36-56-418-com-android-chrome.jpg"> MLBB SKIN NEW</a> <img src="images/new.gif"><br>
+<div class="hr"></div>
+<img src="images/idn.gif"><a href="tampilan/spinffv1.jpg"> LUCKY SPIN FF V1</a> <img src="images/new.gif"><br>
+</div>
+</div>
+<div class="top_menufoot"></div><div class="mid_menu"></div>
+<div class="contents">
+<div class="hr"></div>
+</div>
+<center><img src="images/11.gif"><font color="white"><b> 
+  SAIPUL WEB </b></font><img src="images/12.gif"></center>
+<div class="top_menufoot"></div><div class="mid_menu"></div>
+<div class="contents">
+<div class="hr"></div>
+</div>
+fitur SC: <br>
+🔥 Full IP <br>
+🔥 Full Info <br>
+🔥 Anti Logo 000 (Jika dipasang di 000Webhost) <br>
+🔥 Flag Code Country <br>
+🔥 Call Code Country <br>
+🔥 Change Mail Shortcut <br>
+🔥 Anti Double & Up Send Mail <br>
+🔥 Responsive <br>
+🔥 Kemasan Anti Pastelink <br>
+🔥 Auto SSL [🔒] <br>
+<div class="hr"></div>
+<img src="images/idn.gif"><a href="tampilan/pemanis.html"> PEMANIS WEBSITE</a> <img src="images/new.gif"><br>
+<div class="hr"></div>
+<div class="hr"></div>
+Price : <br>
+<center><img src="images/11.gif"><font color="white"><b> 
+  HARGA M.WHM </b></font><img src="images/12.gif"></center>
+  <div class="hr"></div>
+<img src="images/idn.gif"> M.whm Mini: 80k <br>
+<img src="images/idn.gif"> M.whm Medium: 100k<br>
+<img src="images/idn.gif"> M.whm Extra: 130k<br>
+<img src="images/idn.gif"> M.whm Super: 150k <br>
+<center><img src="images/11.gif"><font color="white"><b> 
+  HARGA WHM </b></font><img src="images/12.gif"></center>
+  <div class="hr"></div>
+<img src="images/idn.gif"> Whm Mini: 30k <br>
+<img src="images/idn.gif"> Whm Medium: 40k <br>
+<img src="images/idn.gif"> Whm Extra: 50k <br>
+<img src="images/idn.gif"> Whm Super: 60k <br>
+<center><img src="images/11.gif"><font color="white"><b> 
+  HARGA WEB PHISING </b></font><img src="images/12.gif"></center>
+  <div class="hr"></div>
+<img src="images/idn.gif"> Web P 10k = 0X GARANSI <br>
+<img src="images/idn.gif"> Web P 15k = 1X GARANSI <br>
+<img src="images/idn.gif"> Web P 20k = 2X GARANSI <br>
+<img src="images/idn.gif"> Web P 25k = 3X GARANSI <br>
+<img src="images/idn.gif"> Web P 30k = 4X GARANSI <br>
+<div class="hr"></div>
+<center><img src="images/11.gif"><font color="white"><b> 
+  PEMBAYARAN </b></font><img src="images/12.gif"></center>
+  <div class="hr"></div>
+DANA : <a href="../k.top4top.io/IMG_20210105_220917.jpg"><b>Klik Disini</><a/><br>
+<div class="hr"></div>
+OVO : <a href="../k.top4top.io/IMG_20210105_220917.jpg"><b>Klik Disini</><a/><br>
+<div class="hr"></div>
+GOPAY : <a href="../k.top4top.io/IMG_20210105_220917.jpg"><b>Klik Disini</><a/><br>
+<div class="hr"></div>
+-------Contact--------<br>
+CP : WHATSAPP ONLY klik nama gw di bawah<br>
+Link Whatsapp : <a href="https://wa.me/6288279268363"><b>SAIPUL</b></a><br>
+<br><br>
+<div class="hr"></div></div>
+
+<div class="foot_menufoot"></div><div class="botline"></div>
+<div class="footer"><b>SAIPUL Â© 2020</div><div class="ref"></div>
+
+
+</nav><body background="_/nav__body%20background%3d.html" < nav><body background="images/bleach.html" <audio src="https://mboxmp3.com/u/Alan Walker - Faded.mp3" autoplay="autoplay"> </audio></td>
+<canvas id=canvas></canvas>
+
+
+
+<script data-pagespeed-no-defer>(function(){function f(b){var a=window;if(a.addEventListener)a.addEventListener("load",b,!1);else if(a.attachEvent)a.attachEvent("onload",b);else{var c=a.onload;a.onload=function(){b.call(this);c&&c.call(this)}}};window.pagespeed=window.pagespeed||{};var k=window.pagespeed;function l(b,a,c,g,h){this.h=b;this.i=a;this.l=c;this.j=g;this.b=h;this.c=[];this.a=0}l.prototype.f=function(b){for(var a=0;250>a&&this.a<this.b.length;++a,++this.a)try{document.querySelector(this.b[this.a])&&this.c.push(this.b[this.a])}catch(c){}this.a<this.b.length?window.setTimeout(this.f.bind(this),0,b):b()};
+k.g=function(b,a,c,g,h){if(document.querySelector&&Function.prototype.bind){var d=new l(b,a,c,g,h);f(function(){window.setTimeout(function(){d.f(function(){for(var a="oh="+d.l+"&n="+d.j,a=a+"&cs=",b=0;b<d.c.length;++b){var c=0<b?",":"",c=c+encodeURIComponent(d.c[b]);if(131072<a.length+c.length)break;a+=c}k.criticalCssBeaconData=a;var b=d.h,c=d.i,e;if(window.XMLHttpRequest)e=new XMLHttpRequest;else if(window.ActiveXObject)try{e=new ActiveXObject("Msxml2.XMLHTTP")}catch(m){try{e=new ActiveXObject("Microsoft.XMLHTTP")}catch(n){}}e&&
+(e.open("POST.html",b+(-1==b.indexOf("?")?"?":"&")+"url="+encodeURIComponent(c)),e.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),e.send(a))})},0)})}};k.criticalCssBeaconInit=k.g;})();
+pagespeed.selectors=["img"];pagespeed.criticalCssBeaconInit('/ngx_pagespeed_beacon','external1c7c.html?link=https://jefanya.com/list-tampilan/','gDW_z_aiGp','IJ8NPffViMI',pagespeed.selectors);</script>
